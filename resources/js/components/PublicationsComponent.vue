873e7422b1f1964c932ee7c66a55d73da8d49e7e
Humberto Viveros <humberto.viverosrios@gmail.com>
2023-10-27 15:16:08 -0300

    Preparando estilos

--- resources/js/components/PublicationsComponent.vue
+++ resources/js/components/PublicationsComponent.vue
@@ -1,5 +1,18 @@
 <template>
-    <h2>Hello World, from component Publications with Vue.js 3 -</h2>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        Publications Component
+                    </div>
+                    <div class="card-body">
+                        <p>Hello World, from component Publications with Vue.js 3 -</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup>
@@ -9,3 +22,24 @@
 <style lang="scss" scoped>
 
 </style>
+
+
+
+
+
+
+
+
+
+
+<!-- <template>
+    <h2>Hello World, from component Publications with Vue.js 3 -</h2>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style> -->
